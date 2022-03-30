971a53b6d40d73fa4c9a18833a01686bf45d4897
omegha-123 <neethufrancis.dec19@gmail.com>
2022-03-30 11:39:20 +0530

    commit

--- engapp/views.py
+++ engapp/views.py
@@ -1,7 +1,18 @@
-
+from django.core.mail import send_mail
+from django.http import HttpResponse
 from django.shortcuts import render
+
+from engforfuture import settings
+
+
 def home(request):
     return render(request,'index.html')
 def about(request):
     return render(request,'about.html')
 # Create your views here.
+def email(request):
+    subject = "Newsletter"
+    message = "Mail sent"
+    recipient = request.POST.get('email')
+    send_mail(subject, message,settings.EMAIL_HOST_USER, [recipient], fail_silently=False)
+    return HttpResponse("mail sent")
--- engforfuture/settings.py
+++ engforfuture/settings.py
@@ -126,3 +126,9 @@ STATICFILES_DIRS = [BASE_DIR/"static"]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER='neethufrancis.dec19@gmail.com'
+EMAIL_HOST_PASSWORD='rose6814'
